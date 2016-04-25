2091ef27b809d33ff8de2c9c9703d7fd4bebb6d5
Bobby Gatlin <bgatlin@mail.valenciacollege.edu>
2016-04-25 12:58:49 -0400

    Final Commit
    Just some finishing touches to fix some spacing issues. Basically just
    nitpicking. I hope you enjoy the game.

--- Bobby Gatlin's Final Project.py
+++ Bobby Gatlin's Final Project.py
@@ -2,7 +2,7 @@ import random
 
 def msg(room):
     if room['msg'] == '':
-        return "You have entered the " + room['name'] + '.'
+        return "\nYou have entered the " + room['name'] + '.'
     else:
         return room['msg']
 
@@ -26,12 +26,12 @@ def get_choice(room,dir):
 def main():
     dirs = (0,0,0,0)
 
-    livingroom = {'name':'Living Room \n You swear everything is laughing as you. \n You feel your own sanity breaking as you bust into laughter along with the sofa and mounted deer head','directions':dirs,'msg':''}
-    kitchen = {'name':'Kitchen and Dining Room \n The room where you had to cut off your own hand after it went bad. \n At least now you have your trusty chainsaw hand','directions':dirs,'msg':''}
-    hall = {'name':'Hall \n You start to feel the darkness overtake you as if possessing your very soul. \n You regain your senses and shake it off','directions':dirs,'msg':''}
-    bathroom = {'name':'Bathroom \n Definitely the scariest room in the whole damn place. \n Whoever last used the john didn\'t flush ','directions':dirs,'msg':''}
-    bedrooma = {'name':'Bedroom #1 \n You pull out your boomstick and lay the smack down on a nasty deadite waiting for an ambush. \n You then decapitate it with your chainsaw hand','directions':dirs,'msg':''}
-    bedroomb = {'name':'Bedroom #2 \n Memories of your beloved Linda playing the piano flood into your mind. \n That was before you had to cut her head off in the woodshed..','directions':dirs,'msg':''}
+    livingroom = {'name':'Living Room \nYou swear everything is laughing as you. \nYou feel your own sanity breaking as you bust into laughter along with the sofa and mounted deer head','directions':dirs,'msg':''}
+    kitchen = {'name':'Kitchen and Dining Room \nThe room where you had to cut off your own hand after it went bad. \nAt least now you have your trusty chainsaw hand','directions':dirs,'msg':''}
+    hall = {'name':'Hall \nYou start to feel the darkness overtake you as if possessing your very soul. \nYou regain your senses and shake it off','directions':dirs,'msg':''}
+    bathroom = {'name':'Bathroom \nDefinitely the scariest room in the whole damn place. \nWhoever last used the john didn\'t flush ','directions':dirs,'msg':''}
+    bedrooma = {'name':'Bedroom #1 \nYou pull out your boomstick and lay the smack down on a nasty deadite waiting for an ambush. \nYou then decapitate it with your chainsaw hand','directions':dirs,'msg':''}
+    bedroomb = {'name':'Bedroom #2 \nMemories of your beloved Linda playing the piano flood into your mind. \nThat was before you had to cut her head off in the woodshed..','directions':dirs,'msg':''}
 
     livingroom['directions'] = (kitchen,0,0,0)
     kitchen['directions'] = (hall,0,livingroom,bedrooma)
@@ -44,26 +44,27 @@ def main():
     room_with_necronomicon = random.choice(rooms)
     necronomicon_found = False
     room = livingroom
-    print('Welcome to Hell once again, Ash. \n' +
-          'You swore you would never step foot into this God-Forsaken cabin ever again but here you are. \n' +
-          'All because somebody screwed up and brought hell back to town by reading a passage out of that cursed Book of the Dead, the Necronomicon Ex Mortis. \n' +
-          'Now, you and your trusty Boomstick have to send those damn Deadites back to where they came from. \n' +
-          'So find the book, say the passage (Klattu Barada Nikto), and end this for good this time.')
+    print('Welcome to Hell once again, Ash.' +
+          '\nYou swore you would never step foot into this God-Forsaken cabin ever again but here you are.' +
+          '\nAll because somebody screwed up and brought hell back to town by reading a passage out of that cursed Book of the Dead, the Necronomicon Ex Mortis.' +
+          '\nNow, you and your trusty Boomstick have to send those damn Deadites back to where they came from.' +
+          '\nSo find the book, say the passage (Klattu Barada Nikto), and end this for good this time.')
 
     while True:
-        if necronomicon_found and room['name'] == 'Living Room \n You swear everything is laughing as you. \n You feel your own sanity breaking as you bust into laughter along with the sofa and mounted deer head':
-            print(' As you step into the room with the book in hand, you can\'t help but to feel the overwhelming presence of evil. \n' +
-            'The whispers of the Deadites, coming to consume every thing you are, is deafening. "We\'ll swallow your soul..We\'ll swallow your soul..." \n' +
-            'You open the book and say the words... Wait, what were those words again... "Klattu Barada Necktie, Nectarine, Nhhhmmmm... \n' +
-            'You cough out a noise that slightly resembles the sound that you can vaguely remember. \n' +
-            'And just like that, everything is quiet. \n' +
-            'You did it Ash. You can now leave this Cabin, seal away the Necronomicon, and never return. \n' +
-            '"Hail to the King, baby!" \n'
-            'The End...or is it?')
+        if necronomicon_found and room['name'] == 'Living Room \nYou swear everything is laughing as you. \nYou feel your own sanity breaking as you bust into laughter along with the sofa and mounted deer head':
+            print('\nAs you step into the room with the book in hand, you can\'t help but to feel the overwhelming presence of evil.' +
+            '\nThe whispers of the Deadites, coming to consume every thing you are, is deafening. "We\'ll swallow your soul..We\'ll swallow your soul..."' +
+            '\nYou open the book and say the words... Wait, what were those words again... "Klattu Barada Necktie, Nectarine, Nhhhmmmm...' +
+            '\nYou cough out a noise that slightly resembles the sound that you can vaguely remember.' +
+            '\nAnd just like that, everything is quiet.' +
+            '\nYou did it Ash. You can now leave this Cabin, seal away the Necronomicon, and never return.' +
+            '\n"Hail to the King, baby!"' +
+            '\n'''
+            '\nThe End...or is it?\n')
             break;
         elif not necronomicon_found and room['name'] == room_with_necronomicon['name']:
               necronomicon_found = True
-              print(msg(room) + ' There it is. The damn book that started this whole mess... \n' +
+              print(msg(room) + '\n''\nThere it is. The damn book that started this whole mess... \n' +
                     'The Necronomicon Ex Mortis...Book of the Dead. Inked in human blood and bound in human flesh, \n' +
                     'This book has ruined your life since the first time you saw it. \n'+
                     'Here\'s your chance to finish this. Get the book to the living room and recite the passage.')
@@ -76,7 +77,7 @@ def main():
 
         stuck = True
         while stuck:
-              dir = input("Which way are you gonna go Ash: N,E,S, or W? ")
+              dir = input("\nWhich way are you gonna go Ash: N,E,S, or W? ")
               choice = get_choice(room,dir)
               if choice == -1:
                   print("So you're a wise guy, eh? Please enter N,E,S, or W? ")
